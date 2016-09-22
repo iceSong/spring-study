b13e9b2c245e7711580d17463c77249596a12b6e
song.liu <song.liu@ele.me>
2016-09-22 19:05:46 +0800

    sql

---
+++ restful-service/src/main/java/hello/DbFactory.java
@@ -0,0 +1,56 @@
+package hello;
+
+import java.sql.*;
+
+/**
+ * TODO
+ *
+ * @author song@liu@ele.me
+ * @date 2016-09-22 18:23
+ */
+public class DbFactory {
+    private static Connection conn;
+    private static String uri = "jdbc:mysql://localhost:3306/demo?"
+            + "user=root&password=BINGsong&useUnicode=true&characterEncoding=UTF8";
+
+    public static Statement getStatement(String sql) {
+        Statement statement = null;
+        getConnection();
+        try {
+            statement = conn.createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            return statement;
+        }
+    }
+
+    public static PreparedStatement prepareStatement(String sql) {
+        PreparedStatement statement = null;
+        getConnection();
+        try {
+            statement = conn.prepareStatement(sql);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            return statement;
+        }
+    }
+
+    private static void getConnection() {
+        if (conn == null) {
+            synchronized (conn) {
+                if (conn == null) {
+                    try {
+                        Class.forName("com.mysql.jdbc.Driver");
+                        conn = DriverManager.getConnection(uri);
+                    } catch (ClassNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+}
--- restful-service/src/main/java/hello/GreetingController.java
+++ restful-service/src/main/java/hello/GreetingController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -21,6 +24,7 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    private String sql = "select * from t_user where name=? and password=? limit 1";
 
     /**
      * requestMapping默认接受所有http请求, 使用method=GET来限制
@@ -28,7 +32,15 @@ public class GreetingController {
      * @return
      */
     @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name, @RequestParam(value = "pwd") String pwd) {
+        PreparedStatement statement = DbFactory.prepareStatement(sql);
+        try {
+            statement.setString(1, name);
+            statement.setString(2, pwd);
+            statement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return new Greeting(counter.incrementAndGet(),
                 String.format(template, name));
     }
